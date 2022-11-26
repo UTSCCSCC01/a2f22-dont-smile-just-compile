ba726a0609c332629f7484e51b331f87809fd8d3
catherine-sun <mailcatherines@gmail.com>
2022-11-26 15:52:42 -0500

    fix location microservice valid uid

--- locationmicroservice/src/main/java/ca/utoronto/utm/mcs/Navigation.java
+++ locationmicroservice/src/main/java/ca/utoronto/utm/mcs/Navigation.java
@@ -22,35 +22,28 @@ public class Navigation extends Endpoint {
     @Override
     public void handleGet(HttpExchange r) throws IOException, JSONException {
         String[] params = r.getRequestURI().toString().split("/");
-        if (params.length != 4 || params[3].isEmpty() || !params[3].matches(".+\\?passengerUid=.+")) {
-            this.sendStatus(r, HttpURLConnection.HTTP_BAD_REQUEST);
-            return;
-        }
-
-        String[] uids = params[3].split("\\?passengerUid=");
-        if (uids.length != 2) {
-            this.sendStatus(r, HttpURLConnection.HTTP_BAD_REQUEST);
+        if (params.length != 4 || params[3].isEmpty() || !params[3].matches("\\d+\\?passengerUid=\\d+")) {
+            this.sendStatus(r, 400);
             return;
         }
 
         try {
-            JSONObject res = new JSONObject();
-            Result result = this.dao.getShortestRoute(uids[0], uids[1]);
-            int status;
+            String[] driverPassenger = params[3].split("\\?passengerUid=");
+            Result result = this.dao.getShortestRoute(driverPassenger[0], driverPassenger[1]);
 
             if (result.hasNext()) {
+                JSONObject res = new JSONObject();
                 Record user = result.next();
                 Map<String, Object> data = user.get("data").asMap();
                 res.put("data", data);
-                status = HttpURLConnection.HTTP_OK;
+                this.sendResponse(r, res, 200);
             } else {
-                status = HttpURLConnection.HTTP_NOT_FOUND;
+                this.sendStatus(r, 404);
             }
-            res.put("status", status);
-            this.sendResponse(r, res, status);
+
         } catch (Exception e) {
             e.printStackTrace();
-            this.sendStatus(r, HttpURLConnection.HTTP_INTERNAL_ERROR);
+            this.sendStatus(r, 500);
         }
     }
 }
--- locationmicroservice/src/main/java/ca/utoronto/utm/mcs/Nearby.java
+++ locationmicroservice/src/main/java/ca/utoronto/utm/mcs/Nearby.java
@@ -1,7 +1,6 @@
 package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.util.Map;
 
 import org.json.*;
@@ -21,21 +20,14 @@ public class Nearby extends Endpoint {
     @Override
     public void handleGet(HttpExchange r) throws IOException, JSONException {
         String[] params = r.getRequestURI().toString().split("/");
-        if (params.length != 4 || params[3].isEmpty() || !params[3].matches(".+\\?radius=\\d+")) {
-            this.sendStatus(r, HttpURLConnection.HTTP_BAD_REQUEST);
-            return;
-        }
-
-        String[] uidRadius = params[3].split("\\?radius=");
-        if (uidRadius.length != 2) {
-            this.sendStatus(r, HttpURLConnection.HTTP_BAD_REQUEST); //should "hi?radius=?radius=0" be accepted? -Catherine
+        if (params.length != 4 || params[3].isEmpty() || !params[3].matches("\\d+\\?radius=\\d+")) {
+            this.sendStatus(r, 400);
             return;
         }
 
         try {
-//            String[] uidRadius = params[3].split("\\?radius=", 2);
-            Result validUid = this.dao.getUserByUid(uidRadius[0]);
-            if (validUid.hasNext()) {
+            String[] uidRadius = params[3].split("\\?radius=");
+            if (this.dao.getUserByUid(uidRadius[0]).hasNext()) {
                 Result result = this.dao.getNearbyDrivers(uidRadius[0], Integer.parseInt(uidRadius[1]));
 
                 if (result.hasNext()) {
@@ -49,16 +41,14 @@ public class Nearby extends Endpoint {
                     } while (result.hasNext());
 
                     res.put("data", data);
-                    this.sendResponse(r, res, HttpURLConnection.HTTP_OK);
-                } else {
-                    this.sendStatus(r, HttpURLConnection.HTTP_NOT_FOUND);
+                    this.sendResponse(r, res, 200);
+                    return;
                 }
-            } else {
-                this.sendStatus(r, HttpURLConnection.HTTP_NOT_FOUND);
             }
+            this.sendStatus(r, 404);
         } catch (Exception e) {
             e.printStackTrace();
-            this.sendStatus(r, HttpURLConnection.HTTP_INTERNAL_ERROR);
+            this.sendStatus(r, 500);
         }
     }
 }
